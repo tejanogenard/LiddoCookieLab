189bb7d752379e550eb0a95875234c98f2c9040e
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2021-01-28 15:12:38 -0800

    initial commit

---
+++ cookielab/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>liddocookielab</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://www.instagram.com/liddocookielab/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          instagram
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
